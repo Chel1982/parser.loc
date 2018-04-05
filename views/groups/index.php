5d0fef4642e2e36ad4aea79cbae0ad4081be10b1
andrew <chel@activist.com>
2018-04-05 16:20:08 +0300

    Убрал id из списка груп

--- views/groups/index.php
+++ views/groups/index.php
@@ -50,7 +50,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             'name',
             'created_at',
             'url_group:url',
