a081146212c388d6a38b87c664e35818497112f4
andrew <chel@activist.com>
2018-04-05 14:42:39 +0300

    Руссифицировал страницу Curls

--- views/curl/create.php
+++ views/curl/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Curl */
 
-$this->title = 'Create Curl';
+$this->title = 'Создать Curl';
 $this->params['breadcrumbs'][] = ['label' => 'Curls', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
