51d1155b279ae995f5381ff84e4cd080db80404a
andrew <chel@activist.com>
2018-04-10 15:59:31 +0300

    Подправил кнопку создать

--- views/curl-auth/index.php
+++ views/curl-auth/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Curl Auth', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Создать Curl Auth', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
