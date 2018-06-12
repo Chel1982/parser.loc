bac58293acf0ed422ab28e0f7893e023a19048c9
andrew <chel@activist.com>
2018-06-12 12:32:45 +0300

    Настройки входа в админку осуществляется через config

--- migrations/migration.sql
+++ migrations/migration.sql
@@ -2,5 +2,8 @@ INSERT INTO `config` (`alias`, `value`) VALUES
 ('name_user', 'admin'),
 ('password', 'fasfw123'),
 ('euro', '73.06'),
-('dollar', '61.81');
+('dollar', '61.81'),
+('authKey', 'test100key'),
+('accessToken', '100-token'),
+('id', '100');
 
--- models/Config.php
+++ models/Config.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\base\NotSupportedException;
 
 /**
  * This is the model class for table "config".
@@ -11,8 +12,12 @@ use Yii;
  * @property string $alias
  * @property string $value
  */
-class Config extends \yii\db\ActiveRecord
+class Config extends \yii\db\ActiveRecord  implements \yii\web\IdentityInterface
 {
+    public $username;
+    public $password;
+    public $authKey;
+    public $accessToken;
 
     /**
      * @inheritdoc
@@ -42,5 +47,84 @@ class Config extends \yii\db\ActiveRecord
             'alias' => 'Алиас',
             'value' => 'Значение',
         ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['alias' => 'id'])->value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return string static
+     */
+    public static function findByUsername($username)
+    {
+
+        if ($username === static::findOne(['alias' => 'name_user'])->value){
+
+            $user = [];
+            $user['username'] = static::findOne(['alias' => 'name_user'])->value;
+            $user['password'] = static::findOne(['alias' => 'password'])->value;
+            $user['authKey'] = static::findOne(['alias' => 'authKey'])->value;
+            $user['accessToken'] = static::findOne(['alias' => 'accessToken'])->value;
+
+            return new static($user);
+
+        }
+    }
+
+    public function setUsername(){
+
+        $this->username = static::findOne(['alias' => 'name_user'])->value;
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return static::findOne(['alias' => 'id'])->value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return static::findOne(['alias' => 'authKey'])->value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return static::findOne(['alias' => 'authKey'])->value === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return static::findOne(['alias' => 'password'])->value === $password;
     }
 }
