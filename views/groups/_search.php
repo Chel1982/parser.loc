18fa0a28762d5d59ea618be6679de7b7f82a7384
andrew <chel@activist.com>
2018-06-13 14:39:45 +0300

    Исправил меню поиска

--- views/groups/_search.php
+++ views/groups/_search.php
@@ -19,10 +19,13 @@ use yii\widgets\ActiveForm;
         ],
     ]); ?>
 
-    <?= $form->field($model, 'name') ?>
+    <?= $form->field($model, 'name',['options' => ['class' => 'col-md-4']]) ?>
 
-    <?= $form->field($model, 'url_group') ?>
+    <?= $form->field($model, 'url_group', ['options' => ['class' => 'col-md-4']]) ?>
 
+    <div class="row">
+        <div class="col-md-4">
+            <?php echo '<label>Выберите дату поиска</label>'; ?>
     <?= DatePicker::widget([
         'model' => $model,
         'attribute' => 'from_date',
@@ -36,6 +39,8 @@ use yii\widgets\ActiveForm;
             'autoclose' => true,
         ]
     ]); ?>
+        </div>
+    </div>
 
 
     <div class="form-group">
